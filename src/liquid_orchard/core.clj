d4e74abea5fd64d8043ef6e7a85d15ddae9b6a0d
Mogens Brødsgaard Lund <sosdamgx@dk8524xsm.sos.eu>
2020-11-13 18:58:11 +0100

    First attempt with goto-definition

---
+++ src/liquid_orchard/core.clj
@@ -0,0 +1,30 @@
+(ns liquid-orchard.core
+  (:require [clojure.string :as str]
+            [liq.editor :as editor]
+            [liq.buffer :as buffer]
+            [liq.modes.clojure-mode :as clojure-mode]
+            [orchard.namespace :as ons]
+            [orchard.meta :as ometa]
+            [orchard.info :as oinfo]
+            [orchard.apropos :as oap]))
+
+(defn orchard-goto-definition
+  [buf]
+  (try
+    (let [namesp (clojure-mode/get-namespace buf)
+          v (re-find #"\w.*\w" (-> buf buffer/left buffer/word))
+          info (meta (ometa/resolve-var (symbol namesp) (symbol v)))
+          p (when (and info (not= (info :file) "NO_SOURCE_FILE")) (.getPath (ons/canonical-source (info :ns))))]
+      (when p
+        (editor/open-file p)
+        (editor/apply-to-buffer #(-> %
+                                     (buffer/beginning-of-buffer (info :line))
+                                     (assoc ::buffer/tow {::buffer/row (info :line) ::buffer/col 1})))))
+    (catch Exception e (str e))))
+
+(defn load-liquid-orchard
+  []
+  (editor/add-key-bindings
+    :clojure-mode
+    :normal
+    {"g" {"D" #(orchard-goto-definition (editor/current-buffer))}}))
